b78d643e51f1d7941db6e50cb178efc1fadeeeac
Syed <syedkhussain1@outlook.com>
2019-05-24 18:01:52 -0700

    Added on click and list function is working

--- client/src/Components/Page_Stuff/BtnAddToList/BtnAddToList.js
+++ client/src/Components/Page_Stuff/BtnAddToList/BtnAddToList.js
@@ -2,8 +2,12 @@ import React from 'react'
 import './BtnAddToList.css'
 
 const BtnAddToList = (props) =>{
+    const showAlert = () => {
+
+        alert("Movie added to list")
+    }
     return (
-        <button onClick={props.addToList} className="btnaddtolist"  text="Add to watch list">
+        <button onClick={props.addToList} onClick={showAlert} className="btnaddtolist"  text="Add to watch list">
             Add to watch list
         </button>
     )
